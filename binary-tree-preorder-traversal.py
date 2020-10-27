e49bd32aeab1e925a0903c0f37e69819a3adf1b7
xichagui <xichagui@gmail.com>
2020-10-27 14:41:19 +0800

    二叉树的前序遍历 -增加非递归方式

--- binary-tree-preorder-traversal.py
+++ binary-tree-preorder-traversal.py
@@ -6,18 +6,38 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
-        if not root:
-            return []
-        
+        # 非递归
         res = []
+        if not root:
+            return res
         
-        def fun(node: TreeNode, res: List):
-            res.append(node.val)
-            if node.left:
-                fun(node.left, res)
-            if node.right:
-                fun(node.right, res)
-        
-        fun(root, res)
+        stack = []
+        node = root
+        while stack or node:
+            while node:
+                res.append(node.val)
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            node = node.right
         
         return res
+
+
+# 递归    
+#         if not root:
+#             return []
+        
+#         res = []
+        
+#         def fun(node: TreeNode, res: List):
+#             res.append(node.val)
+#             if node.left:
+#                 fun(node.left, res)
+#             if node.right:
+#                 fun(node.right, res)
+        
+#         fun(root, res)
+        
+#         return res
+
