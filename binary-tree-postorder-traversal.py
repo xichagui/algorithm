908ce270b3ebc8872db85b3660f28ccdbc5919da
xichagui <xichagui@gmail.com>
2020-10-27 17:02:30 +0800

    二叉树的后序遍历 -非递归

--- binary-tree-postorder-traversal.py
+++ binary-tree-postorder-traversal.py
@@ -7,21 +7,45 @@
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
-        stack = []
-        cur = root
-        per = None
-        while len(stack) > 0 or cur:
-            if cur:
-                stack.append(cur)
-                cur = cur.left
-            else:
-                cur = stack[-1]
-                if cur.right is None or cur.right == per:
-                    res.append(cur.val)
-                    stack.pop()
-                    per = cur
-                    cur = None
-                else:
-                    cur = cur.right
+        if not root:
+            return res
         
+        node = root
+        stack = []
+        pre = None
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+                
+            node = stack.pop()
+            
+            if node.right is None or node.right == pre:
+                res.append(node.val)
+                pre = node
+                node = None
+            else:
+                stack.append(node)
+                node = node.right
+                
         return res
+                
+#         res = []
+#         stack = []
+#         cur = root
+#         per = None
+#         while len(stack) > 0 or cur:
+#             if cur:
+#                 stack.append(cur)
+#                 cur = cur.left
+#             else:
+#                 cur = stack[-1]
+#                 if cur.right is None or cur.right == per:
+#                     res.append(cur.val)
+#                     stack.pop()
+#                     per = cur
+#                     cur = None
+#                 else:
+#                     cur = cur.right
+        
+#         return res
